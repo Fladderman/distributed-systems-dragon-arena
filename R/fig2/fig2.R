e44646344f4490300bc19d294acc640b6e265cd2
zakarias NL <zakarias.nl@gmail.com>
2018-01-25 23:22:10 +0100

    Figured out how to add zeros to shorter vector, didn't really get any further..

--- R/fig2/fig2.R
+++ R/fig2/fig2.R
@@ -5,6 +5,12 @@ server_1_num = read.table(file = '~/DS/server_1_num.tsv', sep = '\t', header = T
 dataframe_server_0 = data.frame(server_0_num)
 dataframe_server_1 = data.frame(server_1_num)
 
+#Make arrays same length by adding zeros
+dataframe_server_0[seq(nrow(dataframe_server_0), nrow(dataframe_server_1)),1] = 0
+
 # adds them after instead of next
 dataframe = rbind(dataframe_server_0=dataframe_server_0[seq(dataframe_server_0)], dataframe_server_1)
+
+# adds them as 2 variables
+dataframe2 = data.frame(dataframe_server_0, dataframe_server_1)
 

1f9abf3354488dd80f14e62ce79e8ec53c80427a
zakarias NL <zakarias.nl@gmail.com>
2018-01-26 16:35:10 +0100

    Plot for figure one is starting to look better

--- R/fig2/fig2.R
+++ R/fig2/fig2.R
@@ -1,6 +1,6 @@
-# read tsv files
-server_0_num = read.table(file = '~/DS/server_0_num.tsv', sep = '\t', header = TRUE)
-server_1_num = read.table(file = '~/DS/server_1_num.tsv', sep = '\t', header = TRUE)
+# Read tsv files:
+server_0_num = read.table(file = '~/Downloads/DATA/dynamic/server_0_num.tsv', sep = '\t', header = TRUE)
+server_1_num = read.table(file = '~/Downloads/DATA/dynamic/server_1_num.tsv', sep = '\t', header = TRUE)
 
 dataframe_server_0 = data.frame(server_0_num)
 dataframe_server_1 = data.frame(server_1_num)
@@ -8,9 +8,17 @@ dataframe_server_1 = data.frame(server_1_num)
 #Make arrays same length by adding zeros
 dataframe_server_0[seq(nrow(dataframe_server_0), nrow(dataframe_server_1)),1] = 0
 
-# adds them after instead of next
-dataframe = rbind(dataframe_server_0=dataframe_server_0[seq(dataframe_server_0)], dataframe_server_1)
+dataframe = data.frame(dataframe_server_0, dataframe_server_1)
 
-# adds them as 2 variables
-dataframe2 = data.frame(dataframe_server_0, dataframe_server_1)
+library(reshape)
+library(ggplot2)
+ggplot(data=dataframe_server_0, aes(x=seq(1,129), y=seq(1,129))) +
+  geom_path(data=dataframe_server_0, aes(X0), colour="lightsalmon3") +  
+  geom_path(data=dataframe_server_1, aes(X0), colour="steelblue2") +
+  coord_flip() +
+  xlim(0, 10) +
+  ylim(0, 129) +
+  labs(y = "Tick", x = "Number of Clients") +
+  scale_y_continuous(breaks = seq(0, 129, by=10)) +
+  theme(panel.background = element_blank())
 

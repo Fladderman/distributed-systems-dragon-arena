b046fb0cf8369cb361a28568be1098ed375767a7
Rink Stiekema <rinkstiekema@hotmail.com>
2018-01-28 17:15:53 +0100

    Updates to fig2

--- R/fig2/fig2.R
+++ R/fig2/fig2.R
@@ -1,6 +1,7 @@
 # Read tsv files:
-server_0_num = read.table(file = 'server_0_num.tsv', sep = '\t', header = TRUE)
-server_1_num = read.table(file = 'server_1_num.tsv', sep = '\t', header = TRUE)
+setwd("E:/Documents/School/Distributed Systems/distributed-systems-dragon-arena/R/fig2")
+server_0_num = read.table(file = 'server_0_num.tsv', sep = '\t', header = FALSE)
+server_1_num = read.table(file = 'server_1_num.tsv', sep = '\t', header = FALSE)
 
 dataframe_server_0 = data.frame(server_0_num)
 dataframe_server_1 = data.frame(server_1_num)
@@ -12,13 +13,13 @@ dataframe = data.frame(dataframe_server_0, dataframe_server_1)
 
 library(reshape)
 library(ggplot2)
-ggplot(data=dataframe_server_0, aes(x=seq(1,129), y=seq(1,129))) +
-  geom_path(data=dataframe_server_0, aes(X0), colour="lightsalmon3") +  
-  geom_path(data=dataframe_server_1, aes(X0), colour="steelblue2") +
+ggplot(data=dataframe_server_0, aes(x=seq(1,130), y=seq(1,130))) +
+  geom_path(data=dataframe_server_0, aes(V1), colour="lightsalmon3") +  
+  geom_path(data=dataframe_server_1, aes(V1), colour="steelblue2") +
   coord_flip() +
   xlim(0, 10) +
-  ylim(0, 129) +
+  ylim(0, 130) +
   labs(y = "Tick", x = "Number of Clients") +
-  scale_y_continuous(breaks = seq(0, 129, by=10)) +
-  theme(panel.background = element_blank())
-
+  scale_y_continuous(breaks = seq(0, 130, by=10)) +
+  geom_hline(yintercept=24, linetype="dashed", color = "grey40") +
+  geom_hline(yintercept=52, linetype="dashed", color = "grey40") 

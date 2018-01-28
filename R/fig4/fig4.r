3ff1c6cdb07ff6837c73041414d80b843db92c5a
zakarias NL <zakarias.nl@gmail.com>
2018-01-28 19:30:39 +0100

    made figure 4

---
+++ R/fig4/fig4.r
@@ -0,0 +1,16 @@
+# Read tsv files:
+#setwd("E:/Documents/School/Distributed Systems/distributed-systems-dragon-arena/R/fig4/")
+server_work = read.table(file = '/Users/ZakariasNL/Documents/STUDY/Master/Distributed-Systems/LAB/distributed-systems-dragon-arena/distributed-systems-dragon-arena/R/fig4/server_0_work.tsv', sep = '\t', header = FALSE)
+
+dataframe_server = data.frame(server_work)
+
+library(ggplot2)
+ggplot(data=dataframe_server, aes(x=seq(1, 699), y=seq(1, 699))) +
+  geom_point(data=dataframe_server, aes(y=V1), colour="royalblue3", size=0.5) +
+  labs(y="Work Time", x="Tick") +
+  scale_y_continuous(breaks = seq(0, 180, by=20), 
+                     expand = c(0, 0), 
+                     limits = c(0, 170)) +
+  theme(panel.background = element_rect(fill = NA),
+        panel.grid.minor = element_line(color = "white"))
+

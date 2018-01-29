788acc25c57645e0282c98161c162e0cb5e443d2
zakarias NL <zakarias.nl@gmail.com>
2018-01-29 16:59:36 +0100

    added labels to fig 5 and 6

--- R/fig5/fig5.r
+++ R/fig5/fig5.r
@@ -4,16 +4,25 @@ server_work_0 = read.table(file = '/Users/ZakariasNL/Documents/STUDY/Master/Dist
 server_work_1 = read.table(file = '/Users/ZakariasNL/Documents/STUDY/Master/Distributed-Systems/LAB/distributed-systems-dragon-arena/distributed-systems-dragon-arena/R/fig5/server_1_work.tsv', sep = '\t', header = FALSE)
 server_work_2 = read.table(file = '/Users/ZakariasNL/Documents/STUDY/Master/Distributed-Systems/LAB/distributed-systems-dragon-arena/distributed-systems-dragon-arena/R/fig5/server_2_work.tsv', sep = '\t', header = FALSE)
 
+length_0 = length(server_work_0$V1)
+
+server_dataframe = data.frame(server_work_0, server_work_1, server_work_2)
+
+server_dataframe["Tick"] = seq(1, length_0)
+colnames(server_dataframe) = c("Server 0", "Server 1", "Server 2", "Tick")
+
+server_melt = melt(server_dataframe, id.vars = "Tick")
+
 library(ggplot2)
-ggplot(data=server_work_0, aes(x=seq(1, 523), y=seq(1, 523))) +
-  geom_point(data=server_work_0, aes(y=V1), colour="green", size=0.5) +
-  geom_point(data=server_work_1, aes(y=V1), colour="lightsalmon3", size=0.5) +
-  geom_point(data=server_work_2, aes(y=V1), colour="royalblue3", size=0.5) +
+ggplot(server_melt) +
+  geom_point(aes(x=Tick, y=value, color=variable), size=0.3) +
   labs(y="Work Time", x="Tick") +
   scale_y_continuous(breaks = seq(0, 180, by=20), 
                      expand = c(0, 0), 
                      limits = c(0, 170)) +
   theme(panel.background = element_rect(fill = NA),
-        panel.grid.minor = element_line(color = "white"))
+        panel.grid.minor = element_line(color = "white")) +
+  guides(colour = guide_legend(override.aes = list(size=3))) +
+  scale_color_manual(values = c("firebrick2", "limegreen", "royalblue4")) 
 
 
--- R/fig6/fig6.r
+++ R/fig6/fig6.r
@@ -9,16 +9,24 @@ server_work_4 = read.table(file = '/Users/ZakariasNL/Documents/STUDY/Master/Dist
 length_0 = length(server_work_0$V1)
 server_work_2 = data.frame(V1=c(0, server_work_2$V1[0:length(server_work_2$V1)]))
 
+server_dataframe = data.frame(server_work_0, server_work_1, server_work_2, 
+                              server_work_3, server_work_4)
+
+server_dataframe["Tick"] = seq(1, length_0)
+colnames(server_dataframe) = c("Server 0", "Server 1", "Server 2", "Server 3", "Server 4", "Tick")
+
+server_melt = melt(server_dataframe, id.vars = "Tick")
+
 library(ggplot2)
-ggplot(data=server_work_0, aes(x=seq(1, length_0), y=seq(1, length_0), label=TRUE, abbr=TRUE)) +
-  geom_point(data=server_work_0, aes(y=V1), colour="royalblue3", size=0.5) +
-  geom_point(data=server_work_1, aes(y=V1), colour="red", size=0.5) +
-  geom_point(data=server_work_2, aes(y=V1), colour="yellowgreen", size=0.5) +
-  geom_point(data=server_work_3, aes(y=V1), colour="green", size=0.5) +
-  geom_point(data=server_work_4, aes(y=V1), colour="magenta4", size=0.5) +
+ggplot(server_melt) +
+  geom_point(aes(x=Tick, y=value, color=variable), size=0.3) +
   labs(y="Work Time", x="Tick") +
-  scale_y_continuous(breaks = seq(0, 180, by=20), 
+  scale_y_continuous(breaks = seq(0, 160, by=20), 
                      expand = c(0, 0), 
-                     limits = c(0, 170)) +
+                     limits = c(0, 160)) +
+  scale_x_continuous(breaks = seq(0, 600, by=100)) +
+  scale_color_manual(values = c("firebrick2", "limegreen", "royalblue4", 
+                                "yellow", "lightsalmon3")) +
   theme(panel.background = element_rect(fill = NA),
-        panel.grid.minor = element_line(color = "white"))
+        panel.grid.minor = element_line(color = "white")) +
+  guides(colour = guide_legend(override.aes = list(size=3)))

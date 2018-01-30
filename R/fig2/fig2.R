eabfb9dd8aac710d58b3c0368c2c39f53fb27c92
zakarias NL <zakarias.nl@gmail.com>
2018-01-30 14:57:45 +0100

    added legend to fig2

--- R/fig2/fig2.R
+++ R/fig2/fig2.R
@@ -11,17 +11,19 @@ dataframe_server_1 = data.frame(server_1_num)
 dataframe_server_0[seq(nrow(dataframe_server_0), nrow(dataframe_server_1)),1] = 0
 
 dataframe = data.frame(dataframe_server_0, dataframe_server_1)
+dataframe["Tick"] = seq(0, length(dataframe_server_0$V1)-1)
+colnames(dataframe) = c("Server 0", "Server 1", "Tick")
+
+dataframe_melt = melt(dataframe, id.vars = "Tick")
 
 library(reshape)
 library(ggplot2)
-ggplot(data=dataframe_server_0, aes(x=seq(1,130), y=seq(1,130))) +
-  geom_path(data=dataframe_server_0, aes(V1), colour="lightsalmon3") +  
-  geom_path(data=dataframe_server_1, aes(V1), colour="steelblue2") +
-  coord_flip() +
-  labs(y = "Tick", x = "Number of Clients") +
-  geom_hline(yintercept=24, linetype="dashed", color = "grey40") +
-  geom_hline(yintercept=52, linetype="dashed", color = "grey40") +
-  scale_y_continuous(breaks = seq(0, 130, by=10), expand = c(0, 0), limits = c(0, 140)) +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, 8), breaks = c(0.0, 2, 4, 6, 8)) + 
+ggplot(dataframe_melt) +
+  geom_path(aes(x=Tick, y=value, color=variable)) +
+  labs(y = "Number of Clients", x = "Tick") +
+  geom_vline(xintercept=24, linetype="dashed", color = "grey40") +
+  geom_vline(xintercept=52, linetype="dashed", color = "grey40") +
+  scale_y_continuous(breaks = seq(0, 8, by=2), expand = c(0, 0), limits = c(0, 8)) +
+  scale_x_continuous(breaks = seq(0, 130, by=10), expand = c(0, 0), limits = c(0, 130)) + 
   theme(panel.background = element_blank())
   
